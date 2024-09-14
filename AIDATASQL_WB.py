4f7959c66bb49c364ee227e1064a073a431760e9
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2024-09-14 15:50:22 +0500

    Update AIDATASQL_WB.py

--- AIDATASQL_WB.py
+++ AIDATASQL_WB.py
@@ -11,7 +11,7 @@ from sqlalchemy import create_engine, text
 from github import Github, GithubException
 
 # GitHub repository details
-GITHUB_TOKEN = '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'  # Replace with your new GitHub Personal Access Token
+GITHUB_TOKEN = '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'  # Replace with your new GitHub Personal Access Token
 GITHUB_REPO = 'AmmarJamshed/Data-manip-with-Pandas-and-other-basic-libraires'  # Replace with your GitHub repo name
 
 # SQLite connection
