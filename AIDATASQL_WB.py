53b90eceb4c44a6c0dcd0cb599f9f40041418719
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2024-09-16 16:26:41 +0500

    Update AIDATASQL_WB.py

--- AIDATASQL_WB.py
+++ AIDATASQL_WB.py
@@ -9,7 +9,7 @@ from sqlalchemy import create_engine
 from github import Github, GithubException
 
 # GitHub repository details - Ensure to set this as an environment variable
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')  # Replace with your environment variable
+GITHUB_TOKEN = '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'  # Replace with your environment variable
 GITHUB_REPO = 'AmmarJamshed/Data-manip-with-Pandas-and-other-basic-libraires'  # Only pass the 'owner/repository' format
 
 # SQLite connection
